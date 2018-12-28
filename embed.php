7ff14dea7ae4285faa856a4bc471bd613f67453a
TextError <savinraul1983@yahoo.com>
2018-12-28 17:10:21 +0000

    simple math

--- embed.php
+++ embed.php
@@ -12,9 +12,15 @@
     $name = 'Edwin'; //string
     $number = 100;  //number
 
+    //Math
+    $number1 = 10;
+    $number2 = 15;
+
+    $result = $number1 + $number2;
+
     //Concatenate
     $concatenate = $name . ' ' . $number;
   ?>
 
-  <h1> <?php echo $concatenate ?> <h1>
+  <h1> <?php echo $result ?> <h1>
 </body>
