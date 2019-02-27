b83ade0fdb0fdf51349d59e27811888c93f126d4
TextError <savinraul1983@yahoo.com>
2019-02-27 09:51:06 +0000

    pass 1 obj as parameter to class, default values, get diffrent birthday

--- dragonstack/index.js
+++ dragonstack/index.js
@@ -1,7 +1,19 @@
 const Dragon = require('./dragon');
 
-const fooey = new Dragon(new Date(), 'fooey');
-const baloo = new Dragon(new Date(), 'baloo');
+const fooey = new Dragon({
+  birthdate:new Date(),
+  nickname: 'fooey'
+});
+const baloo = new Dragon({
+  birthdate: new Date(),
+  nickname: 'baloo'
+});
 
-console.log('fooey', fooey);
-console.log('baloo', baloo);
+const mimar = new Dragon();
+
+setTimeout(() => {
+  const gooby = new Dragon();
+  console.log('gooby', gooby);
+}, 3000);
+
+console.log('mimar', mimar)
