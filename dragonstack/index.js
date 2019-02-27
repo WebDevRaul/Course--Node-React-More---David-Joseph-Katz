43bed4522ef5a1837db4d2e5e6ff755d8fe8a1c3
TextError <savinraul1983@yahoo.com>
2019-02-27 09:32:58 +0000

    first dragon (dragon class)

--- dragonstack/index.js
+++ dragonstack/index.js
@@ -1,1 +1,7 @@
-console.log('hello node');
+const Dragon = require('./dragon');
+
+const fooey = new Dragon(new Date(), 'fooey');
+const baloo = new Dragon(new Date(), 'baloo');
+
+console.log('fooey', fooey);
+console.log('baloo', baloo);
