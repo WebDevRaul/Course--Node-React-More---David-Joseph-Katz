e910188b620043a446ebf7735eb5f3108e588a14
TextError <savinraul1983@yahoo.com>
2019-03-01 23:32:05 +0000

    testing pool,, finish later

--- dragonstack/backend/databasePool.js
+++ dragonstack/backend/databasePool.js
@@ -4,3 +4,10 @@ const databaseConfiguration = require('./secrets/databaseConfiguration');
 const pool = new Pool(databaseConfiguration);
 
 module.exports = pool;
+
+
+pool.query('SELECT * FROM generation', (error, response) => {
+  if (error) return console.log('error', error);
+
+  console.log('response.rows', response)
+});
