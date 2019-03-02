6572198acc8a0e59e675ebc2157e1c41895eb776
TextError <savinraul1983@yahoo.com>
2019-03-02 23:45:13 +0000

    dragon table

--- dragonstack/backend/app/generation/index.js
+++ dragonstack/backend/app/generation/index.js
@@ -1,4 +1,4 @@
-const Dragon = require('../dragon');
+const Dragon = require('../dragon/dragon');
 const { REFRESH_RATE, SECONDS } = require('../config');
 
 const refreshRate = REFRESH_RATE * SECONDS;
