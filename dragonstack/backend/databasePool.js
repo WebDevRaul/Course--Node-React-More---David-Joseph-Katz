96585a8c369cb4a4c91e57218f7c0de52572f7e7
TextError <savinraul1983@yahoo.com>
2019-03-03 11:04:29 +0000

    fix typos

--- dragonstack/backend/databasePool.js
+++ dragonstack/backend/databasePool.js
@@ -9,5 +9,5 @@ module.exports = pool;
 pool.query('SELECT * FROM generation', (error, response) => {
   if (error) return console.log('error', error);
 
-  console.log('response.rows', response)
+  console.log('response.rows', response.rows);
 });
