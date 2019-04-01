5462f991c44bffc76249e15c7d51a9e3be60cdbc
TextError <savinraul1983@yahoo.com>
2019-04-01 18:21:56 +0100

    redux store,reducer

--- dragonstack/frontend/src/index.js
+++ dragonstack/frontend/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { createStore } from 'redux';
 import { render } from 'react-dom';
 import Generation from './components/Generation';
 import Dragon from './components/Dragon';
 
-import './index.css'
+import './index.css';
+
+const DEFAULT_GENERATION = {getElementById: '', expiration: ''}
+
+const generationReducer = () => {
+  return { generation: DEFAULT_GENERATION }
+}
+
+const store = createStore( generationReducer );
 
 render(
   <div>
