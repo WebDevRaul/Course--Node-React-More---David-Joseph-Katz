edcd4a59f520fdf4e26bb64f5b9b70a61d200c68
TextError <savinraul1983@yahoo.com>
2019-04-12 14:25:02 +0100

    account table, accout api, new acc request

---
+++ dragonstack/backend/app/api/account.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const AccountTable = require('../account/table');
+
+const router = new Router();
+
+router
+  .post('/signup', (req, res, next) => {
+    const { username, password } = req.body;
+
+    AccountTable.storeAccount({ username, password })
+      .then(() => res.json({ message: 'success!' }))
+      .catch(err => next(err));
+  });
+
+
+module.exports = router
